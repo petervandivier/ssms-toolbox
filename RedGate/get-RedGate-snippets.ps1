f8aba5e16ee6c9cc07a7adc1effc8627a32ff87e
petervandivier <peter.vandivier@gmail.com>
2025-07-16 10:48:24 -0400

    🗑️ rm hidden snippets. add SqlLib cleanup

--- RedGate/get-RedGate-snippets.ps1
+++ RedGate/get-RedGate-snippets.ps1
@@ -3,14 +3,15 @@ Push-Location $PSScriptRoot
 
 $config = Get-Content .\RedGate.config.json | ConvertFrom-Json
 $PublicSnippetFolder = Invoke-Expression "Resolve-Path `"$($config.SnippetFolder)`""
-$PrivateSnippetRepo = Invoke-Expression "Resolve-Path `"$($config.HiddenSnippets.StashFolder)`""
+# $PrivateSnippetRepo = Invoke-Expression "Resolve-Path `"$($config.HiddenSnippets.StashFolder)`""
 $PrivateSnippetSlug = $config.HiddenSnippets.FilterSlug
 
 Get-ChildItem -LiteralPath $PublicSnippetFolder -File | ForEach-Object {
     $snip = Get-Content -LiteralPath "$($_.FullName)" | ConvertFrom-Json
     Write-Verbose $_
     if($snip.description.EndsWith($PrivateSnippetSlug)){
-        Copy-Item -LiteralPath "$($_.FullName)"  -Destination "$PrivateSnippetRepo/$($_.Name)"
+        # Copy-Item -LiteralPath "$($_.FullName)"  -Destination "$PrivateSnippetRepo/$($_.Name)"
+        Write-Warning "😅 Oops! $($_.Name) may be a private file. You should probably add back private snippet support!" 
     }else{
         Copy-Item -LiteralPath "$($_.FullName)"  -Destination "./10/Snippets/$($_.Name)"
         Set-Content ../Snippets/$($snip.prefix).sql "/*$([Environment]::NewLine)$($snip.description)$([Environment]::NewLine)*/$([Environment]::NewLine)"
@@ -25,8 +26,11 @@ Add-Type -AssemblyName Microsoft.VisualBasic
     Get-ChildItem -LiteralPath "$_" -Filter *.json | ForEach-Object {
         $exists = Test-Path "$PublicSnippetFolder\$($_.Name)"
         if(-not $exists){
-            Write-Warning "Deleting $($_.Name)"
+            $shortName = $_.Name.Split('-')[0]
+            Write-Warning "Deleting $shortName"
+            $sqlLibFile = Resolve-Path "../Snippets/${shortName}.sql"
             [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile($_.FullName,'OnlyErrorDialogs','SendToRecycleBin')
+            [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile($sqlLibFile,'OnlyErrorDialogs','SendToRecycleBin')
         }
     }
 }
